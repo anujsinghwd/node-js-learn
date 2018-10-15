553dd9d58ff3b06ac6373d23f23abb411ce15484
anujsinghwd <anuj_singh@outlook.in>
2018-10-15 23:55:41 +0530

    nody final

--- nody/app.js
+++ nody/app.js
@@ -1,6 +1,31 @@
 const yargs = require('yargs');
 const notes = require('./notes');
-const argv = yargs.argv;
+const titleObj = {
+    describe: 'Title of note', 
+    demand: true,
+    alias: 't'
+};
+
+const bodyObj = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+                .command('add', 'Add a new note', {
+                    title: titleObj,
+                    body: bodyObj
+                })
+                .command('list', 'List all notes')
+                .command('read', 'Read a note', {
+                    title: titleObj
+                })
+                .command('remove', 'Remove a note', {
+                    title: titleObj
+                })
+                .help()
+                .argv;
 
 var command = argv._[0];
 
